77c78ced96d56172dff0e36a7d10b252cdcee32b
anaadhi <anaadhimongia9@gmail.com>
2023-03-17 22:39:19 +0530

    added build

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const ejs = require('ejs');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// middleware
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => res.render('index.ejs'));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
